e7f69606d7fbb53587e843d1e730b841916be2da
Chirag Karia <trichiragkaria@gmail.com>
2023-06-13 01:52:04 -0400

    Change build_dir to out_dir in after/plugin/vimtix.lua due to vimtex updates

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -2,7 +2,7 @@ vim.cmd("syntax enable")
 vim.g.vimtex_view_method = 'zathura'
 
 vim.g.vimtex_compiler_latexmk = {
-    build_dir = 'build',
+    out_dir = 'build',
     options = {
         '-shell-escape',
         '-synctex=1'
