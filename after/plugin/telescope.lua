761449714330b94d4c05ea7d462c321e216fd9dd
Chirag Karia <trichiragkaria@gmail.com>
2024-01-19 20:47:11 -0500

    Update telescope to include files specified in .gitignore in file search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,7 +11,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 
 vim.keymap.set('n', '<leader>pp', function()
-    builtin.find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } });
+    builtin.find_files({ find_command = { 'rg', '--files', '--hidden', '--no-ignore-vcs', '-g', '!.git' } });
 end)
 
 vim.keymap.set('n', '<leader>ps', function()
