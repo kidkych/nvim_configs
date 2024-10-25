2d4b418fb5d1af3d00fdab630948ce9f1bb218c7
Chirag Karia <trichiragkaria@gmail.com>
2024-10-25 18:17:22 -0400

    Force enable osc52 clipboard provider

--- lua/kychdev/set.lua
+++ lua/kychdev/set.lua
@@ -8,7 +8,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -27,3 +26,16 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+local osc52 = require("vim.ui.clipboard.osc52")
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ["+"] = osc52.copy("+"),
+        ["*"] = osc52.copy("*"),
+    },
+    paste = {
+        ["+"] = osc52.paste("+"),
+        ["*"] = osc52.paste("*"),
+    },
+}
